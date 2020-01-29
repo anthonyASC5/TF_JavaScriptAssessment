bb46342b9d7ae63b9eeece1c4f1f7805ff950265
anthonyASC5 <52748939+anthonyASC5@users.noreply.github.com>
2020-01-28 20:16:07 -0500

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,26 @@
 function fizzBuzz(start, end, skip){
     // Your code here!
+    var answers = []
+    for (let i = start; i <= end; i++) {
+var stringskip = skip.toString()
+var iT = i.toString()
+//^^ to make it into a string
+
+        if ( !iT.includes(stringskip)) {
+            answers.push(i)
+        
+        }
+    }       
+
+
+
+    return (answers)
+
+ 
 }
+console.log(fizzBuzz(1,19,1))
+//(1,2,3,4,6,7,8,9)
+//Anthony Lall Lewis
 
 // DON'T EDIT THE LINE BELOW THIS COMMENT
 module.exports = fizzBuzz;
