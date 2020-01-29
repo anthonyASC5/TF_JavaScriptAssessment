fab60f20d956f4460e1fe120ca3deba0ea17a8de
anthonyASC5 <52748939+anthonyASC5@users.noreply.github.com>
2020-01-28 20:17:45 -0500

    Update oddoneout.js

--- oddoneout.js
+++ oddoneout.js
@@ -1,6 +1,20 @@
 function oddOneOut(arrOfInts){
     // Your code here!
+    // console.log(arrOfInts);
+    for (let i = 0; i < arrOfInts.length; i++) {
+        var vish = arrOfInts[i];
+
+        if (vish % 2 == 1){
+           return vish
+        }
+     
+
+    }
+    return -1
 }
+console.log(oddOneOut([14,16,120,12]));
+//Made by Anthony Lall Lewis
+  
 
 
 // DON'T EDIT THE LINE BELOW THIS COMMENT
